1c98422a9b736928f2a5e189936b829a7ee0883f
AhmedSalheia <ahmedsalheia.as@gmail.com>
2022-03-23 17:59:40 +0200

    updates

--- src/views/Chat/index.vue
+++ src/views/Chat/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <div class="chat">
+    <Contacts></Contacts>
+    <attachment></attachment>
+  </div>
 </template>
 
 <script>
-export default {};
+import Attachment from "@/components/Chat/Attachment.vue";
+import Contacts from "@/components/Chat/Contact.vue";
+export default {
+  components: {
+    Contacts,
+    Attachment,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.chat {
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  position: relative;
+  overflow: hidden;
+}
+</style>
