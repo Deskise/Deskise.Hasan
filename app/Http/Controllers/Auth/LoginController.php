d4e90238ca8cdc78322b6510baa1239cd0f27045
Ahmed Salheia <ahmedsalheia.as@gmail.com>
2022-09-04 16:24:11 +0300

    updates

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -50,11 +50,11 @@ class LoginController extends Controller
         $user = Admin::where('email',$request->input('email'))->first();
         if ($user)
         {
-            if (!\Hash::check($request->input('password'),$user->password)) return redirect()->back()->withErrors(['email'=>'Wrong Username Or Password']);
+            if (!\Hash::check($request->input('password'),$user->password)) return redirect()->back()->withErrors(['email'=>'These credentials do not match our records.']);
             \Auth::login($user);
             return redirect()->route('admin.dashboard');
         }
-        return redirect()->back()->withErrors(['email'=>'Wrong Username Or Password']);
+        return redirect()->back()->withErrors(['email'=>'These credentials do not match our records.']);
     }
 
     public function logout(Request $request)
